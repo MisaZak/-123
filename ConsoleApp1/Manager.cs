d9192ac45bce9866bf80e3e797f56b4861a4ba54
adamo <adamo@S>
2026-05-21 17:09:26 +0300

    Добавили класс

--- ConsoleApp1/Manager.cs
+++ ConsoleApp1/Manager.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    internal class Class1
+    internal class Manager
     {
-        public static int a = 1;
-        public static string ak = "hello";
-        
+        public static int a = 9;
     }
 }
